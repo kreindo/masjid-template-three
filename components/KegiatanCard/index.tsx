a627aada9513ad01695dea0ee1d2b13b3c5e3043
kreindo <ahmadsan4p@gmail.com>
2022-09-22 13:23:57 +0800

    finalizisyon

--- components/KegiatanCard/index.tsx
+++ components/KegiatanCard/index.tsx
@@ -1,10 +1,11 @@
-import { ImageCard } from 'components/ImageCard';export const KegiatanCard = () => {
+import { ImageCard } from 'components/ImageCard';
+export const KegiatanCard = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center mx-8 sm:mx-0">
         <div className="mb-[50px] flex justify-center items-center text-center">
           <h1 className="text-white text-3xl font-bold">
-            Galeri Masjid Nurussalam
+            Galeri Masjid Raydhatul Jannah
           </h1>
         </div>
         <ImageCard />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Layout } from '../components/Layout';
 import { Hero } from '../components/Hero';
 import { BangRevaJie } from '../components/Container';
-import Image from 'next/image';
 import { KegiatanCard } from 'components/KegiatanCard';
 import { Footer } from 'components/Footer';
 import { KegiatanBox } from 'components/KegiatanBox';
@@ -14,21 +13,13 @@ export default function Home() {
           <Hero />
         </Layout>
       </div>
-      {/* <div className="relative h-[50px]">
-        <Image
-          src={'/assets/images/divider1.svg'}
-          alt={'divider 1'}
-          layout={'fill'}
-          objectFit={'cover'}
-        />
-      </div> */}
       <BangRevaJie>
         <div id="Kegiatan" className="my-[100px] text-white">
           <KegiatanCard />
         </div>
         <div className="mb-[100px]">
           <div className="mb-[50px] flex justify-center items-center">
-            <h1 className="text-white text-3xl font-bold">Kegiatan</h1>
+            <h1 className="text-white text-3xl font-bold">Kegiatan Masjid</h1>
           </div>
           <KegiatanBox title={'Foo Bar Boo'} dummy={false}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex iste
@@ -53,7 +44,7 @@ export default function Home() {
         </div>
       </BangRevaJie>
       <div id="Map" className="mb-[50px] flex justify-center items-center">
-        <h1 className="text-white text-3xl font-bold">Lokasi</h1>
+        <h1 className="text-white text-3xl font-bold">Lokasi Masjid</h1>
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254172.44553857786!2d120.08321543281248!3d-5.500827999999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbc08611c908c4f%3A0xe2f1c9e93907d5f5!2sNURUL%20SALAM!5e0!3m2!1sen!2sid!4v1663638512280!5m2!1sen!2sid"
